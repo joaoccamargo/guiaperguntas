2e1a9338570c4467489c9f56990d370ddccef137
João Camargo <jclaudio00_12@hotmail.com>
2023-06-19 11:15:28 -0300

    adjusted database logs

--- src/database/database.js
+++ src/database/database.js
@@ -2,5 +2,6 @@ import Sequelize from "sequelize";
 
 export const connection = new Sequelize('guiaperguntas', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false //logs
 });
